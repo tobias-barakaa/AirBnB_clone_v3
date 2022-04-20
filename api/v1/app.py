e4d2acb2cdfc5addb7b5299e8f9955cff1eef702
pericles001 <periclesadjovi@outlook.fr>
2022-04-20 02:11:37 +0100

    Added doc to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Import a blueprint and runs Flask """
 
-from flask import Flask, jsonify, request, make_response
-from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
 from os import getenv
+
 from flasgger import Swagger
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,30 +21,34 @@ app.config['SWAGGER'] = {
         ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
         ('Access-Control-Allow-Credentials', "true"),
     ],
-    'specs':[
+    'specs': [
         {
             'version': '1.0.0',
             'title': 'HBNB API',
             'endpoint': 'v1_views',
             'description': 'HBNB REST API',
-            'route':'/v1/views/',
+            'route': '/v1/views/',
         }
     ]
 }
 
 swagger = Swagger(app)
 
+
 @ap.teardown_appcontext
 def teardown_db(exception):
     """Closes storage session"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 errors"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == "__main__":
+    """Runs Flask"""
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
 
