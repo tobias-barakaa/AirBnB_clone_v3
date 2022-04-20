927845ec5b16601f7a5f8830915b9cb71f06f9a4
pericles001 <periclesadjovi@outlook.fr>
2022-04-20 02:24:48 +0100

    Added new line

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ status"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
