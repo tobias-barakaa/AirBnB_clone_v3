8c903fb33a86980cf4414932dad28fbbe0850378
pericles001 <periclesadjovi@outlook.fr>
2022-04-20 02:06:25 +0100

    Filled file with configs

---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Index"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ status"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ stats"""
+    return jsonify(storage.count())
