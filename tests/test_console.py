f5e14d0f8f1dbe4325709146465a8303ccbf6cbb
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-01 04:25:58 -0700

    conso

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,6 +4,7 @@ Contains the class TestConsoleDocs
 """
 
 import console
+import models
 import inspect
 import pep8
 import unittest
@@ -39,3 +40,8 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+    def test_console_module_docstring_exists(self):
+        """Test for the existence of the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
