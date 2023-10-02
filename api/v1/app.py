e30d8b9922ce5f1cdcb6aabe68ac73098a91dd7b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-02 14:04:28 -0700

    https-modify

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,8 @@ script to running the first app
 
 from flask import Flask, make_response, jsonify
 from models import storage
-from api.v1.views import app_views
-from flask_cors import CORS
 from os import getenv
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
