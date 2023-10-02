03d74be9b6fbf422e937b38baa1efb06fcd1231e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-02 15:36:48 -0700

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,3 +17,19 @@ def get_status():
         JSON response with the status "OK"
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """
+    Retrieves the number of each object type.
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
