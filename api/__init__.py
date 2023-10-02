5a29931b9517975d4ae80e15430a00b2a157267e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-02 11:24:40 -0700

    Committing my changes in .app.py.swp

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-"""
-Create a Blueprint with the URL prefix "/api/v1"
-"""
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import views from your index.py
-from . import index
