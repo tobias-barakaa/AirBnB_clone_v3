b1860a5e91a9e5eed84d27b16a1a6a65bb5d05a5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-02 15:52:36 -0700

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,18 @@
 """
 running first ap
 """
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+
+classes = [Amenity, City, Place, Review, State, User]
+names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
 
 @app_views.route('/status', methods=['GET'])
@@ -22,14 +31,10 @@ def get_status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """
-    Retrieves the number of each object type.
+    Retrieves the number of each object.
     """
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-    return jsonify(stats)
+    list = {}
+    for k in range(len(classes)):
+        list[names[k]] = storage.count(classes[k])
+
+    return jsonify(list)
