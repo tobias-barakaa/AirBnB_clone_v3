350d7a6ee688821ebfb4de309a0c146beb07adef
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-10-02 19:55:04 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-from flask import Flask
+"""app"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-"""
-running first app
-"""
-app = Flask(__name__)
+from os import getenv
+from flask_cors import CORS
 
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_storage(exception):
-    """
-    This function is called when the Flask app context is torn down.
-
-    Args:
-        exception (Exception): The exception, if any, that triggered the teardown.
-
-    Returns:
-        None
-    """
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
 
-if __name__ == "__main__":
-    # Get the host and port values from environment variables or use defaults
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
 
-    # Run the Flask application with the specified host and port
-    app.run(host=host, port=port)
+@app.errorhandler(404)
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
