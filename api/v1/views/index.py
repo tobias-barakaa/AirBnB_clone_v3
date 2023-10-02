5408a2ac9e9c58bd525285b7b91e28bf981aacd3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-02 15:03:33 -0700

    app

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,10 +6,12 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """
-    This function defines a route that returns a JSON response with the status "OK".
+    This function defines a route that returns a
+    JSON response with the status "OK".
 
     Returns:
         JSON response with the status "OK"
